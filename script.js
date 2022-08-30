edd46360b5c38611c9fdfce52a170de67abb9548
Abigail <neneojingwa@gmail.com>
2022-08-30 02:43:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let submit = document.getElementById('btn')
+// submit.addEventListener('click',()=>{
+//     alert('try put password wee strong o')
+// })
+let p = document.getElementsByTagName('p')
+let inpt = document.getElementsByTagName('input')
+for(let ipt of inpt){
+    console.log(ipt.value)
+}
