30a87106bf54d3644b270e3c3db83233583def94
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-30 03:23:29 +0100

    modified the html file using js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let inpt = document.getElementsByTagName('input')
 let para = document.querySelector('section p')
 para.innerText = 'lets keep working hard, lets keep adding values , life moves on \n Made des changes on my js file'
 let step2 = document.querySelector('.step2')
-step2.innerHTML = '<i>take a decision to be serious with God</i> \n Made des changes on my js file'
+step2.innerHTML = '<i>take a decision to be serious with God</i> \n <b> this so important </b> Made des changes on my js file'
 // printing out the value of the users input
 for(let ipt of inpt){
     console.log(ipt.value)
