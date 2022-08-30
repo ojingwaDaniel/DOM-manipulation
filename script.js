c7181a92e9e0a6fdb8382b341bae60572eedf92f
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-30 03:20:59 +0100

    innerHTML

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ let submit = document.getElementById('btn')
 let p = document.getElementsByTagName('p')
 let inpt = document.getElementsByTagName('input')
 let para = document.querySelector('section p')
-para.innerText = 'lets keep working hard, lets keep adding values , life moves on'
+para.innerText = 'lets keep working hard, lets keep adding values , life moves on \n Made des changes on my js file'
+let step2 = document.querySelector('.step2')
+step2.innerHTML = '<i>take a decision to be serious with God</i> \n Made des changes on my js file'
 // printing out the value of the users input
 for(let ipt of inpt){
     console.log(ipt.value)
