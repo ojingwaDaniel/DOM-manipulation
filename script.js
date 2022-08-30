045de2c55f0f54d754f9c714811553c1bc684e38
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-30 03:07:52 +0100

    innerText

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let submit = document.getElementById('btn')
 // })
 let p = document.getElementsByTagName('p')
 let inpt = document.getElementsByTagName('input')
+let para = document.querySelector('section p')
+para.innerText = 'lets keep working hard, lets keep adding values , life moves on'
+// printing out the value of the users input
 for(let ipt of inpt){
     console.log(ipt.value)
 }
